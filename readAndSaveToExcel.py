a15e5ad56a017fdd1282b5ba9eb7ace1f7519f3b
Jonathan D <asfarasicantell@outlook.com>
2022-11-18 16:37:42 -0600

    Added Test 5 and 6

--- readAndSaveToExcel.py
+++ readAndSaveToExcel.py
@@ -4,7 +4,7 @@ from classes.gateway import GatewayService
 myPort = port_controller.PortController(port="COM7", measurementsPerTest=15, numberOfTests=20)
 gateway = GatewayService.create(GatewayService.VAMIA_V0)
 
-myPort.establishFileNames(measurementFileName="Measuremets4.xlsx", statisticsFileName="Statistics4.xlsx")
+myPort.establishFileNames(measurementFileName="Measuremets6.xlsx", statisticsFileName="Statistics6.xlsx")
 list1 = [i*20 for i in range(11)]
 list2 = [i*40+240 for i in range(9)]
 myPort.setTestValues(list1+list2)
